833d0a570bfb77f6d3c2a4e271842151fb0fbefc
jiewj <jie_wj@126.com>
2016-09-01 16:59:53 +0800

    init project structure

---
+++ src/components/Main.js
@@ -0,0 +1,39 @@
+require('normalize.css/normalize.css');
+require('styles/App.scss');
+
+// 获取图片相关的数据
+var imageDatas = require('../data/imageDatas.json');
+
+// 利用自执行函数，将图片名信息转成图片URL路径信息
+imageDatas = (function genImageURl(imageDatasArr) {
+  for (var i = 0,j = imageDatasArr.length;i < j; i++){
+    var singleImageData = imageDatasArr[i];
+
+    singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+
+    imageDatasArr[i] = singleImageData;
+  }
+
+  return imageDatasArr;
+})(imageDatas);
+
+
+import React from 'react';
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <section className="stage">
+        <section className="img-sec">
+        </section>
+        <nav className="controller-nav">
+        </nav>
+      </section>
+
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
